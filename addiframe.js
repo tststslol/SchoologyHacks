c85b6332f63d2b2b8b35631e58bba22912166c69
Roger Fan <rogerjoeyfan@gmail.com>
2020-08-05 17:35:00 -0700

    let's see if this work

--- addiframe.js
+++ addiframe.js
@@ -3,11 +3,15 @@
     toInsertIframe.src = prompt("Please enter the URL for the IFrame (you can still switch to another tab during this time):");
     toInsertIframe.width = "100%";
     toInsertIframe.height = "500px";
-    document.querySelectorAll("#tinymce").appendChild(toInsertIframe);
+    document.getElementById("tinymce").appendChild(toInsertIframe);
 
     if(prompt("You should be able to see the iframe now in the input box. Would you like to continue? (\"yes\" to continue, anything else for no.)") !== "yes"){
         alert("The insertion was cancelled. Reload to clear the input box, or just use backspace.");
         return;
     }
+
+    /* 
+    document.head.appendChild(document.createElement('script')).src = 'https://bubbybabur.github.io/SchoologyHacks/addiframe.js'
+    */
     
 })();
