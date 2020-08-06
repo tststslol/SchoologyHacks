494940d03b1345c08a3dc58539bb5e98cfb10d2b
Roger Fan <rogerjoeyfan@gmail.com>
2020-08-05 17:28:04 -0700

    Add the custom js

---
+++ addiframe.js
@@ -0,0 +1,13 @@
+(function(){
+    let toInsertIframe = document.createElement("iframe");
+    toInsertIframe.src = prompt("Please enter the URL for the IFrame (you can still switch to another tab during this time):");
+    toInsertIframe.width = "100%";
+    toInsertIframe.height = "500px";
+    document.querySelectorAll("#tinymce").appendChild(toInsertIframe);
+
+    if(prompt("You should be able to see the iframe now in the input box. Would you like to continue? (\"yes\" to continue, anything else for no.)") !== "yes"){
+        alert("The insertion was cancelled. Reload to clear the input box, or just use backspace.");
+        return;
+    }
+    
+})();
