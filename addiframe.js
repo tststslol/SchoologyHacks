3a22b6964bf1927596d8e68092e51b9f4d0013d2
Roger Fan <rogerjoeyfan@gmail.com>
2020-08-05 19:08:44 -0700

    Y E S

--- addiframe.js
+++ addiframe.js
@@ -1,14 +1,18 @@
 (function(){
     let toInsertIframe = document.createElement("iframe");
     toInsertIframe.src = prompt("Please enter the URL for the IFrame (you can still switch to another tab during this time):");
-    toInsertIframe.width = "100%";
-    toInsertIframe.height = "500px";
+    toInsertIframe.width = `${prompt("Enter a number (in pixels) for how wide the iframe should be: ")}px`;
+    toInsertIframe.height = `${prompt("Enter a number (in pixels) for how high the iframe should be: ")}px`;
     document.getElementById("edit-body_ifr").contentDocument.getElementById("tinymce").appendChild(toInsertIframe);
 
-    if(prompt("You should be able to see the iframe now in the input box. Would you like to continue? (\"yes\" to continue, anything else for no.)") !== "yes"){
-        alert("The insertion was cancelled. Reload to clear the input box, or just use backspace.");
-        return;
-    }
+    requestAnimationFrame( () => {
+        if (prompt("You should be able to see the iframe now in the input box. Would you like to continue? (\"yes\" to continue, anything else for no.)") !== "yes") {
+            alert("The insertion was cancelled. Reload to clear the input box, or just use backspace.");
+            return;
+        }
+    });
+
+    
 
     /* 
     document.head.appendChild(document.createElement('script')).src = 'https://bubbybabur.github.io/SchoologyHacks/addiframe.js'
