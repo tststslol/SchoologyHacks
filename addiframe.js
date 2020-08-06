e0de8728392e7cb0040b0fe27118c1b80edbdc30
Roger Fan <rogerjoeyfan@gmail.com>
2020-08-05 17:46:35 -0700

    ok iframe time

--- addiframe.js
+++ addiframe.js
@@ -3,7 +3,7 @@
     toInsertIframe.src = prompt("Please enter the URL for the IFrame (you can still switch to another tab during this time):");
     toInsertIframe.width = "100%";
     toInsertIframe.height = "500px";
-    document.getElementById("tinymce").appendChild(toInsertIframe);
+    document.getElementById("edit-body_ifr").contentDocument.getElementById("tinymce").appendChild(toInsertIframe);
 
     if(prompt("You should be able to see the iframe now in the input box. Would you like to continue? (\"yes\" to continue, anything else for no.)") !== "yes"){
         alert("The insertion was cancelled. Reload to clear the input box, or just use backspace.");
